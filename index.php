6231581ce458939bdabaca0066729f963f5dc1ac
understxnd <alex.targonskiy@yandex.ru>
2023-02-03 11:32:12 +0500

    1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require('dbconnect.php');
+
+$result = $conn->query('SELECT * FROM `visit`');
+while($row = $result->fetch())// получаем все строки в цикле по одной
+{
+    echo '<p>Запись id='.$row['id'].'. Начало сеанса: '.$row['session_time'].'. Конец сеанса: '.$row['end_of_session'].'. id клиента: '.$row['id_client'].'. id фотографа: '.$row['id_photographer'].'. Тип сессии: '.$row['type_session'].'</p>';// выводим данные
+}
+
