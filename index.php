896e1448e147aa4a649534efb5fa5a5a1166cae2
understxnd <alex.targonskiy@yandex.ru>
2023-02-03 12:00:38 +0500

    1

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 require('dbconnect.php');
 
 $result = $conn->query('SELECT * FROM `visit`');
-while($row = $result->fetch())// получаем все строки в цикле по одной
+while($row = $result->fetch())
 {
-    echo '<p>Запись id='.$row['id'].'. Начало сеанса: '.$row['session_time'].'. Конец сеанса: '.$row['end_of_session'].'. id клиента: '.$row['id_client'].'. id фотографа: '.$row['id_photographer'].'. Тип сессии: '.$row['type_session'].'</p>';// выводим данные
+    echo '<p>Запись id='.$row['id'].'. Начало сеанса: '.$row['session_time'].'. Конец сеанса: '.$row['end_of_session'].'. 
+    id клиента: '.$row['id_client'].'. id фотографа: '.$row['id_photographer'].'. Тип сессии: '.$row['type_session'].'</p>';
 }
 
